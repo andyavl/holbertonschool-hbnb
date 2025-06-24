e70285c3e52e99f6dc0d4555efd6a185bfb87367
andyavl <andyavl@hotmail.com>
2025-06-23 22:17:58 -0400

    3. Amenity Endpoints

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
+
 
 class HBnBFacade:
     def __init__(self):
@@ -28,6 +30,26 @@ class HBnBFacade:
             return None
         user.update(data)
         return user
+
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        amenity.update(amenity_data)
+        return amenity
+
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
